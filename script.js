eff50fc9eb1ffbb6b172135efbc3f67e963ff435
Adnan Farra <62692354+AdnanFarra1@users.noreply.github.com>
2022-02-26 12:15:03 +0200

    added function updaterate

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ var years = document.getElementById("years").value;
 var interest = principal * years * rate /100;
 var year = new Date().getFullYear()+parseInt(years);
 
-
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval + "%";
+}        
 
 function compute()
 {
