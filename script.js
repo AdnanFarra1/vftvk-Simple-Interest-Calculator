e3e44bc63dc53c053f8de68e81f7e1f829fad84e
Adnan Farra <62692354+AdnanFarra1@users.noreply.github.com>
2022-02-26 13:17:22 +0200

    added validation to compute function

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
 var interest = principal * years * rate /100;
 var year = new Date().getFullYear()+parseInt(years);
 
@@ -12,7 +9,15 @@ function updateRate()
 
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
     
+    if (principal == " ") {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+
+    }
 }
-        
+         
